84f07156d38a97dbf568b570c07a2a87a6099587
Gabriela Alexandra Moldovan <gabi_250@live.com>
2018-01-14 16:20:35 +0000

    Jump to the exit statement in unopt.

--- stackmap_checker/stmap.c
+++ stackmap_checker/stmap.c
@@ -1,72 +1,142 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include "stmap.h"
 
-stack_map_t* create_stack_map(uint8_t *start_addr) {
-   stack_map_t *sm = (stack_map_t *)malloc(sizeof(stack_map_t));
-   char *addr = (char *)start_addr;
-   size_t header_size = sizeof(uint8_t) * 2 + sizeof(uint16_t) + 3 * sizeof(uint32_t);
-   memcpy(sm, (void *)addr, header_size);
-   sm->version = *(uint8_t *)addr;
-   addr += header_size;
-   sm->stack_size_records =
-      (stack_size_record_t *)malloc(sizeof(stack_size_record_t) * sm->num_func);
-   for (size_t i = 0; i < sm->num_func; ++i) {
-      stack_size_record_t *rec = sm->stack_size_records + i;
-      memcpy(rec, (void *)addr, sizeof(stack_size_record_t));
-      addr += sizeof(stack_size_record_t);
-   }
-   sm->constants = (uint64_t *)malloc(sizeof(uint64_t) * sm->num_const);
-   for (size_t i = 0; i < sm->num_const; ++i) {
-      uint64_t *constant = sm->constants + i;
-      *constant = *(uint64_t *)addr;
-      addr += sizeof(uint64_t);
-   }
-   sm->sm_records =
-      (stack_map_record_t *)malloc(sizeof(stack_map_record_t) * sm->num_rec);
-   size_t record_size = sizeof(uint64_t) + sizeof(uint32_t) + 2 * sizeof(uint16_t);
-   for (size_t i = 0; i < sm->num_rec; ++i) {
-      stack_map_record_t *rec = sm->sm_records + i;
-      // copy the first 4 fields
-      memcpy(rec, addr, record_size);
-      addr += record_size;
-      rec->locations =
-         (location_t *)malloc(sizeof(location_t) * rec->num_locations);
-      size_t loc_size =
-         2 * sizeof(uint8_t) + 3 * sizeof(uint16_t) + sizeof(int32_t);
-      for (size_t j = 0; j < rec->num_locations; ++j) {
-         location_t *loc = rec->locations + j;
-         memcpy(loc, addr, loc_size);
-         addr += loc_size;
-      }
-      if ((rec->num_locations * sizeof(location_t)) % 8 != 0) {
-         addr += 4; // padding
-      }
-      addr += 2; // padding
-      rec->num_liveouts = *(uint16_t *)addr;
-      addr += 2;
-      rec->liveouts = (liveout_t *)malloc(sizeof(liveout_t) * rec->num_liveouts);
-      size_t liveout_size = sizeof(uint16_t) + 2 * sizeof(uint8_t);
-      for (int j = 0; j < rec->num_liveouts; ++j) {
-         liveout_t *liveout = rec->liveouts + j;
-         memcpy(liveout, addr, liveout_size);
-         addr += liveout_size;
-      }
-      if ((rec->num_liveouts * sizeof(liveout_t) + 4) % 8 != 0) {
-         addr += 4; // padding
-      }
-   }
-   return sm;
+stack_map_t* create_stack_map(uint8_t *start_addr)
+{
+    stack_map_t *sm = (stack_map_t *)malloc(sizeof(stack_map_t));
+    char *addr = (char *)start_addr;
+    size_t header_size = sizeof(uint8_t) * 2 + sizeof(uint16_t) + 3 * sizeof(uint32_t);
+    memcpy(sm, (void *)addr, header_size);
+    sm->version = *(uint8_t *)addr;
+    addr += header_size;
+    sm->stk_size_records =
+        (stack_size_record_t *)malloc(sizeof(stack_size_record_t) * sm->num_func);
+    for (size_t i = 0; i < sm->num_func; ++i) {
+        stack_size_record_t *rec = sm->stk_size_records + i;
+        memcpy(rec, (void *)addr, sizeof(stack_size_record_t));
+        addr += sizeof(stack_size_record_t);
+    }
+    sm->constants = (uint64_t *)malloc(sizeof(uint64_t) * sm->num_const);
+    for (size_t i = 0; i < sm->num_const; ++i) {
+        uint64_t *constant = sm->constants + i;
+        *constant = *(uint64_t *)addr;
+        addr += sizeof(uint64_t);
+    }
+    sm->stk_map_records =
+        (stack_map_record_t *)malloc(sizeof(stack_map_record_t) * sm->num_rec);
+    size_t record_size = sizeof(uint64_t) + sizeof(uint32_t) + 2 * sizeof(uint16_t);
+    for (size_t i = 0; i < sm->num_rec; ++i) {
+        stack_map_record_t *rec = sm->stk_map_records + i;
+        // copy the first 4 fields
+        memcpy(rec, addr, record_size);
+        addr += record_size;
+        rec->locations =
+            (location_t *)malloc(sizeof(location_t) * rec->num_locations);
+        size_t loc_size =
+            2 * sizeof(uint8_t) + 3 * sizeof(uint16_t) + sizeof(int32_t);
+        for (size_t j = 0; j < rec->num_locations; ++j) {
+            location_t *loc = rec->locations + j;
+            memcpy(loc, addr, loc_size);
+            addr += loc_size;
+        }
+        if ((rec->num_locations * sizeof(location_t)) % 8 != 0) {
+            addr += 4; // padding
+        }
+        addr += 2; // padding
+        rec->num_liveouts = *(uint16_t *)addr;
+        addr += 2;
+        rec->liveouts = (liveout_t *)malloc(sizeof(liveout_t) * rec->num_liveouts);
+        size_t liveout_size = sizeof(uint16_t) + 2 * sizeof(uint8_t);
+        for (int j = 0; j < rec->num_liveouts; ++j) {
+            liveout_t *liveout = rec->liveouts + j;
+            memcpy(liveout, addr, liveout_size);
+            addr += liveout_size;
+        }
+        if ((rec->num_liveouts * sizeof(liveout_t) + 4) % 8 != 0) {
+            addr += 4; // padding
+        }
+    }
+    return sm;
 }
 
-void free_stack_map(stack_map_t *sm) {
-   free(sm->stack_size_records);
-   free(sm->constants);
-   for (size_t i = 0; i < sm->num_rec; ++i) {
-      stack_map_record_t *rec = sm->sm_records + i;
-      free(rec->locations);
-      free(rec->liveouts);
-   }
-   free(sm->sm_records);
-   free(sm);
+void print_locations(stack_map_t *stack_map, void *frame_addr, uint64_t *regs)
+{
+    for (size_t i = 0; i < stack_map->num_rec; ++i) {
+        stack_map_record_t rec = stack_map->stk_map_records[i];
+        if (rec.num_locations) {
+            printf("Record %zu:\n", i);
+        }
+        for (size_t j = 0; j < rec.num_locations; ++j) {
+            location_type type = rec.locations[j].kind - 1;
+            if (type == REGISTER) {
+                uint16_t reg_num = rec.locations[j].dwarf_reg_num;
+                printf("\t[REGISTER %hu] Loc %zu, value is %lu\n", reg_num, j,
+                       regs[reg_num]);
+                if (regs[reg_num]) {
+                    printf("\t*(int *)regs[reg_num] = %d\n", *(int *)regs[reg_num]);
+                }
+            } else if (type == DIRECT) {
+                int *addr = (int *)frame_addr + rec.locations[j].offset;
+                printf("\t[DIRECT] Loc %zu, value is %d @ %p\n", j,
+                        *addr, (void *)addr);
+                if (addr) {
+                    printf("\t*addr = %d\n", *addr);
+                }
+            } else if (type == INDIRECT) {
+                int *addr = (int *)frame_addr + rec.locations[j].offset;
+                printf("\t[INDIRECT] Loc %zu, value is %d @ %p\n", j,
+                        *addr, (void *)addr);
+            } else if (type == CONSTANT) {
+                int32_t offset = rec.locations[j].offset;
+                printf("\t[CONSTANT] Loc %zu, value is %lu\n", j,
+                        stack_map->constants[offset]);
+            } else {
+                printf("unk\n");
+            }
+        }
+    }
+}
+
+void print_constants(stack_map_t *stack_map)
+{
+    for (size_t i = 0; i < stack_map->num_const; ++i) {
+        uint64_t constant = stack_map->constants[i];
+        printf("[CONSTANT] Constant %zu, value is %lu\n", i,
+                constant);
+    }
+}
+
+void print_liveouts(stack_map_t *stack_map, uint64_t *regs)
+{
+    for (size_t i = 0; i < stack_map->num_rec; ++i) {
+        stack_map_record_t rec = stack_map->stk_map_records[i];
+        if (rec.num_liveouts) {
+            printf("Record %zu:\n", i);
+        }
+        for (size_t j = 0; j < rec.num_liveouts; ++j) {
+            liveout_t liveout = rec.liveouts[j];
+            uint16_t reg_num = liveout.dwarf_reg_num;
+            printf("\t[REGISTER %hu] Liveout %zu, value is %lu\n", reg_num, j,
+                   regs[reg_num]);
+
+            if (regs[reg_num]) {
+                printf("\t*(int *)regs[reg_num] = %d\n", *(int *)regs[reg_num]);
+            }
+        }
+    }
+}
+
+void free_stack_map(stack_map_t *sm)
+{
+    free(sm->stk_size_records);
+    free(sm->constants);
+    for (size_t i = 0; i < sm->num_rec; ++i) {
+        stack_map_record_t *rec = sm->stk_map_records + i;
+        free(rec->locations);
+        free(rec->liveouts);
+    }
+    free(sm->stk_map_records);
+    free(sm);
 }
--- stackmap_checker/trace.c
+++ stackmap_checker/trace.c
@@ -9,30 +9,29 @@
 #include "stmap.h"
 
 void *get_addr(char *section_name) {
-   int fd = open("trace", O_RDONLY);
-   void *data = mmap(NULL,
-                     lseek(fd, 0, SEEK_END), // file size
-                     PROT_READ,
-                     MAP_SHARED, fd, 0);
-   close(fd);
-   Elf64_Ehdr *elf = (Elf64_Ehdr *) data;
-   Elf64_Shdr *shdr = (Elf64_Shdr *) (data + elf->e_shoff);
-   char *strtab = (char *)(data + shdr[elf->e_shstrndx].sh_offset);
-   for(int i = 0; i < elf->e_shnum; i++) {
-      if (strcmp(section_name, &strtab[shdr[i].sh_name]) == 0) {
-         printf("Found section: %s\n", &strtab[shdr[i].sh_name]);
-         return (void *) shdr[i].sh_offset + 0x400000;
-      }
-   }
-   return NULL;
+    int fd = open("trace", O_RDONLY);
+    void *data = mmap(NULL,
+                      lseek(fd, 0, SEEK_END), // file size
+                      PROT_READ,
+                      MAP_SHARED, fd, 0);
+    close(fd);
+    Elf64_Ehdr *elf = (Elf64_Ehdr *) data;
+    Elf64_Shdr *shdr = (Elf64_Shdr *) (data + elf->e_shoff);
+    char *strtab = (char *)(data + shdr[elf->e_shstrndx].sh_offset);
+    for(int i = 0; i < elf->e_shnum; i++) {
+        if (strcmp(section_name, &strtab[shdr[i].sh_name]) == 0) {
+            printf("Found section: %s\n", &strtab[shdr[i].sh_name]);
+            return (void *) shdr[i].sh_offset + 0x400000;
+        }
+    }
+    return NULL;
 }
 
 void unopt();
 
 void __guard_failure(uint64_t sm_id) {
-   char buffer[8];
-   volatile uint64_t r[16];
-   asm volatile("mov %%rax,%0\n"
+    uint64_t r[16];
+    asm volatile("mov %%rax,%0\n"
                  "mov %%rcx,%1\n"
                  "mov %%rdx,%2\n"
                  "mov %%rbx,%3\n"
@@ -42,62 +41,65 @@ void __guard_failure(uint64_t sm_id) {
                  "mov %%rdi,%7\n" : "=r"(r[0]), "=r"(r[1]), "=r"(r[2]),
                                     "=r"(r[3]), "=r"(r[4]), "=r"(r[5]),
                                     "=r"(r[6]), "=r"(r[7]) : : );
-   asm volatile("mov %%r8,%0\n"
-                "mov %%r9,%1\n"
-                "mov %%r10,%2\n"
-                "mov %%r11,%3\n"
-                "mov %%r12,%4\n"
-                "mov %%r13,%5\n"
-                "mov %%r14,%6\n"
-                "mov %%r15,%7\n" : "=r"(r[8]), "=r"(r[9]), "=r"(r[10]),
-                                   "=r"(r[11]), "=r"(r[12]), "=r"(r[13]),
-                                   "=r"(r[14]), "=r"(r[15]) : : );
-   printf("Guard %lu failed!\n", sm_id);
-   void *stack_map_addr = get_addr(".llvm_stackmaps");
-   if (!stack_map_addr) {
-      printf(".llvm_stackmaps section not found. Exiting.\n");
-      exit(1);
-   }
-   stack_map_t *stack_map = create_stack_map(stack_map_addr);
-   stack_map_record_t rec = stack_map->sm_records[sm_id];
-   void *bp = __builtin_frame_address(1);
-   printf("Locations:\n");
-   for (size_t j = 0; j < rec.num_locations; ++j) {
-      location_type type = rec.locations[j].kind - 1;
-      if (type == REGISTER) {
-         int reg_num = rec.locations[j].dwarf_reg_num;
-         printf("[REGISTER %d] Loc %zu, value is %lu\n", reg_num, j, r[reg_num]);
-      } else if (type == DIRECT) {
-         int *addr = bp + stack_map->sm_records[sm_id].locations[j].offset;
-         printf("[DIRECT] Loc %zu, value is %d @ %p\n", j,
-                *addr, (void *)addr);
-      }
-   }
-   if (rec.num_liveouts) {
-      printf("Liveouts:\n");
-      for (size_t j = 0; j < rec.num_liveouts; ++j) {
-         liveout_t liveout = rec.liveouts[j];
-         int reg_num = liveout.dwarf_reg_num;
-         printf("[REGISTER %d] Liveout %zu, value is %d\n", reg_num, j,
-                *(int *)r[reg_num]);
-      }
-   }
-   uint64_t fun_addr = 0;
-   for (size_t j = 0; j < stack_map->num_rec; ++j) {
-      stack_size_record_t s_rec = stack_map->stack_size_records[j];
-      if ((void *)s_rec.fun_addr == (void *) unopt) {
-         // useless check, need to work out where to jump
-         fun_addr = s_rec.fun_addr;
-      }
-   }
-   free_stack_map(stack_map);
-   asm volatile("jmp *%0" : : "r"(fun_addr));
+    asm volatile("mov %%r8,%0\n"
+                 "mov %%r9,%1\n"
+                 "mov %%r10,%2\n"
+                 "mov %%r11,%3\n"
+                 "mov %%r12,%4\n"
+                 "mov %%r13,%5\n"
+                 "mov %%r14,%6\n"
+                 "mov %%r15,%7\n" : "=r"(r[8]), "=r"(r[9]), "=r"(r[10]),
+                                    "=r"(r[11]), "=r"(r[12]), "=r"(r[13]),
+                                    "=r"(r[14]), "=r"(r[15]) : : );
+    printf("Guard %lu failed!\n", sm_id);
+    void *stack_map_addr = get_addr(".llvm_stackmaps");
+    if (!stack_map_addr) {
+        printf(".llvm_stackmaps section not found. Exiting.\n");
+        exit(1);
+    }
+    stack_map_t *stack_map = create_stack_map(stack_map_addr);
+    void *bp = __builtin_frame_address(1);
+    print_locations(stack_map, bp, r);
+    print_liveouts(stack_map, r);
+    print_constants(stack_map);
+
+    uint64_t fun_addr = 0;
+    for (size_t j = 0; j < stack_map->num_rec; ++j) {
+        stack_size_record_t s_rec = stack_map->stk_size_records[j];
+        if ((void *)s_rec.fun_addr == (void *) unopt) {
+            // useless check, need to work out where to jump
+            fun_addr = s_rec.fun_addr;
+        }
+    }
+    free_stack_map(stack_map);
+
+    asm volatile("mov %0,%%rax\n"
+                 "mov %1,%%rcx\n"
+                 "mov %2,%%rdx\n"
+                 "mov %3,%%rbx\n"
+                 "mov %4,%%rsp\n"
+                 "mov %5,%%rbp\n"
+                 "mov %6,%%rsi\n"
+                 "mov %7,%%rdi\n" : : "r"(r[0]), "r"(r[1]), "r"(r[2]),
+                                      "r"(r[3]), "r"(r[4]), "r"(r[5]),
+                                      "r"(r[6]), "r"(r[7]) : );
+    asm volatile("mov %0,%%r8\n"
+                 "mov %1,%%r9\n"
+                 "mov %2,%%r10\n"
+                 "mov %3,%%r11\n"
+                 "mov %4,%%r12\n"
+                 "mov %5,%%r13\n"
+                 "mov %6,%%r14\n"
+                 "mov %7,%%r15\n" : : "r"(r[8]), "r"(r[9]), "r"(r[10]),
+                                      "r"(r[11]), "r"(r[12]), "r"(r[13]),
+                                      "r"(r[14]), "r"(r[15]) : );
+    asm volatile("movl $0x5,-0x4(%rbp)");
+    asm volatile("jmp *%0" : : "r"(fun_addr + 15));
 }
 
 void unopt() {
     int x = 2;
-    printf("unopt: %d @ %p\n", x, &x);
-    exit(1);
+    exit(x);
 }
 
 void trace() {
